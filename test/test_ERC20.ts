8ee1a3ca8a32758121c8139eaee5229a9ee4df02
papermoonzhou <zhou@papermoon.io>
2025-06-02 12:40:20 +0800

    erc20 test done

--- test/test_ERC20.ts
+++ test/test_ERC20.ts
@@ -9,17 +9,17 @@ describe("ERC20", function () {
   // Test fixture with contract and signers
   let token: PVMERC20;
   let owner: Signer;
-  let addr1: Signer;
-  let addr2: Signer;
+  let wallet1: Signer;
+  let wallet2: Signer;
 
   const name = "Test Token";
   const symbol = "TST";
-  const initialSupply = ethers.parseEther("1000");
+  const initialSupply = ethers.parseEther("10000");
 
   // Deploy a concrete implementation of the abstract ERC20 contract before each test
   before(async function () {
-    [owner, addr1] = await ethers.getSigners();
-    addr2 = ethers.Wallet.createRandom(ethers.getDefaultProvider());
+    [owner, wallet1] = await ethers.getSigners();
+    wallet2 = ethers.Wallet.createRandom(ethers.getDefaultProvider());
 
     // Deploy a test implementation of ERC20
     const ERC20Factory = await ethers.getContractFactory("PVMERC20");
@@ -46,176 +46,118 @@ describe("ERC20", function () {
 
   describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
-      const amount = ethers.parseEther("50");
+      const amount = ethers.parseEther("100");
       const ownerAddress = await owner.getAddress();
-      const addr1Address = await addr1.getAddress();
+      const wallet1Address = await wallet1.getAddress();
 
-      // Transfer from owner to addr1
-      await token.transfer(addr1Address, amount);
+      // Transfer from owner to wallet1
+      await token.transfer(wallet1Address, amount);
 
       // Check balances reflect the transfer
-      expect(await token.balanceOf(addr1Address)).to.equal(amount);
+      expect(await token.balanceOf(wallet1Address)).to.equal(amount);
       expect(await token.balanceOf(ownerAddress)).to.equal(
         initialSupply - amount,
       );
     });
 
     it("Should update balances after transfers", async function () {
-      const amount = ethers.parseEther("100");
-      const ownerAddress = await owner.getAddress();
-      const addr1Address = await addr1.getAddress();
-      const addr2Address = await addr2.getAddress();
+      const wallet1Address = await wallet1.getAddress();
+      const wallet2Address = await wallet2.getAddress();
+      const amount = await token.balanceOf(wallet1Address);
 
-      // Transfer from owner to addr1
-      await token.transfer(addr1Address, amount);
-      console.log("=======================");
-      console.log(await token.balanceOf(addr1Address));
-      console.log(await token.balanceOf(addr2Address));
-
-
-      // Transfer from addr1 to addr2
-      //   const contract = new Contract(await token.getAddress(), token.interface, addr1);
-      await token.connect(addr1).transfer(addr2Address, amount / ethers.toBigInt(2));
-
-      console.log("=======================");
-      console.log(await token.balanceOf(addr1Address));
-      console.log(await token.balanceOf(addr2Address));
-
-
+      await token.connect(wallet1).transfer(wallet2Address, amount / ethers.toBigInt(2));
       // Check balances
-      //   expect(await token.balanceOf(ownerAddress)).to.equal(initialSupply - amount);
-      expect(await token.balanceOf(addr1Address)).to.equal(amount);
-      console.log("=======================");
-      console.log(await token.balanceOf(addr1Address));
-      console.log(await token.balanceOf(addr2Address));
-      expect(await token.balanceOf(addr2Address)).to.equal(amount);
+      expect(await token.balanceOf(wallet1Address)).to.equal(amount / ethers.toBigInt(2));
+      expect(await token.balanceOf(wallet2Address)).to.equal(amount / ethers.toBigInt(2));
     });
   });
 
-  // describe("Allowance", function () {
-  //     it("Should update allowance when approve is called", async function () {
-  //         const amount = ethers.parseEther("100");
-  //         const ownerAddress = await owner.getAddress();
-  //         const addr1Address = await addr1.getAddress();
+  describe("Allowance", function () {
+    it("Should update allowance when approve is called", async function () {
+      const amount = ethers.parseEther("100");
+      const ownerAddress = await owner.getAddress();
+      const allowanceWallet = ethers.Wallet.createRandom(ethers.getDefaultProvider());
+      const allowanceWalletAddress = await allowanceWallet.getAddress();
 
-  //         await token.approve(addr1Address, amount);
+      await token.approve(allowanceWalletAddress, amount);
 
-  //         expect(await token.allowance(ownerAddress, addr1Address)).to.equal(amount);
-  //     });
+      expect(await token.allowance(ownerAddress, allowanceWalletAddress)).to.equal(amount);
+    });
 
-  //     it("Should allow transferFrom with sufficient allowance", async function () {
-  //         const amount = ethers.parseEther("100");
-  //         const ownerAddress = await owner.getAddress();
-  //         const addr1Address = await addr1.getAddress();
-  //         const addr2Address = await addr2.getAddress();
+    it("Should allow transferFrom with sufficient allowance", async function () {
+      const amount = ethers.parseEther("100");
+      const ownerAddress = await owner.getAddress();
+      const wallet1Address = await wallet1.getAddress();
+      const receiverWallet = ethers.Wallet.createRandom(ethers.getDefaultProvider());
+      const receiverWalletAddress = await receiverWallet.getAddress();
 
-  //         await token.approve(addr1Address, amount);
-  //         await token.connect(addr1).transferFrom(ownerAddress, addr2Address, amount);
+      await token.approve(wallet1Address, amount);
+      await token.connect(wallet1).transferFrom(ownerAddress, receiverWalletAddress, amount);
 
-  //         expect(await token.balanceOf(addr2Address)).to.equal(amount);
-  //         expect(await token.allowance(ownerAddress, addr1Address)).to.equal(0);
-  //     });
+      expect(await token.balanceOf(receiverWalletAddress)).to.equal(amount);
+      expect(await token.allowance(ownerAddress, wallet1Address)).to.equal(0);
+    });
 
-  //     it("Should not decrease allowance for max uint256 approval", async function () {
-  //         const maxAllowance = ethers.constants.MaxUint256;
-  //         const amount = ethers.parseEther("100");
-  //         const ownerAddress = await owner.getAddress();
-  //         const addr1Address = await addr1.getAddress();
-  //         const addr2Address = await addr2.getAddress();
+    it("Should not decrease allowance for over approval", async function () {
 
-  //         await token.approve(addr1Address, maxAllowance);
-  //         await token.connect(addr1).transferFrom(ownerAddress, addr2Address, amount);
+      const ownerAddress = await owner.getAddress();
+      const wallet1Address = await wallet1.getAddress();
+      const initialBalance = await token.balanceOf(ownerAddress);
+      const amount = initialBalance + 1n;
 
-  //         expect(await token.allowance(ownerAddress, addr1Address)).to.equal(maxAllowance);
-  //     });
+      await token.approve(wallet1Address, amount);
 
-  //     it("Should fail when trying to transferFrom more than allowed", async function () {
-  //         const allowance = ethers.parseEther("99");
-  //         const amount = ethers.parseEther("100");
-  //         const ownerAddress = await owner.getAddress();
-  //         const addr1Address = await addr1.getAddress();
-  //         const addr2Address = await addr2.getAddress();
+      expect(await token.balanceOf(ownerAddress)).to.equal(initialBalance);
+    });
 
-  //         await token.approve(addr1Address, allowance);
+    it("Should fail when trying to transferFrom more than allowed", async function () {
+      const allowance = ethers.parseEther("99");
+      const amount = ethers.parseEther("100");
+      const ownerAddress = await owner.getAddress();
+      const wallet1Address = await wallet1.getAddress();
+      const wallet2Address = await wallet2.getAddress();
 
-  //         await expect(
-  //             token.connect(addr1).transferFrom(ownerAddress, addr2Address, amount)
-  //         ).to.be.revertedWith("ERC20InsufficientAllowance");
-  //     });
-  // });
+      await token.approve(wallet1Address, allowance);
 
-  // describe("Internal functions", function () {
-  //     it("Should mint tokens correctly", async function () {
-  //         const amount = ethers.parseEther("500");
-  //         const addr1Address = await addr1.getAddress();
-  //         const initialSupply = await token.totalSupply();
+      await expect(
+        token.connect(wallet1).transferFrom(ownerAddress, wallet2Address, amount)
+      ).to.be.reverted;
+    });
+  });
 
-  //         await token.mint(addr1Address, amount);
 
-  //         expect(await token.balanceOf(addr1Address)).to.equal(amount);
-  //         expect(await token.totalSupply()).to.equal(initialSupply.add(amount));
-  //     });
+  describe("Edge cases", function () {
+    it("Should fail when transferring to the zero address", async function () {
+      const amount = ethers.parseEther("100");
 
-  //     it("Should burn tokens correctly", async function () {
-  //         const amount = ethers.parseEther("200");
-  //         const ownerAddress = await owner.getAddress();
-  //         const initialSupply = await token.totalSupply();
+      await expect(
+        token.transfer(ethers.ZeroAddress, amount)
+      ).to.be.reverted;
+    });
 
-  //         await token.burn(ownerAddress, amount);
+    it("Should fail when approving the zero address as spender", async function () {
+      const amount = ethers.parseEther("100");
 
-  //         expect(await token.balanceOf(ownerAddress)).to.equal(initialSupply.sub(amount));
-  //         expect(await token.totalSupply()).to.equal(initialSupply.sub(amount));
-  //     });
+      await expect(
+        token.approve(ethers.ZeroAddress, amount)
+      ).to.be.reverted;
+    });
 
-  //     it("Should fail when minting to the zero address", async function () {
-  //         const amount = ethers.parseEther("100");
+    it("Should handle zero value transfers correctly", async function () {
+      const wallet1Address = await wallet1.getAddress();
+      const initialBalance = await token.balanceOf(wallet1Address);
 
-  //         await expect(
-  //             token.mint(ethers.constants.AddressZero, amount)
-  //         ).to.be.revertedWith("ERC20InvalidReceiver");
-  //     });
+      await token.transfer(wallet1Address, 0);
 
-  //     it("Should fail when burning from the zero address", async function () {
-  //         const amount = ethers.parseEther("100");
+      expect(await token.balanceOf(wallet1Address)).to.equal(initialBalance);
+    });
 
-  //         await expect(
-  //             token.burn(ethers.constants.AddressZero, amount)
-  //         ).to.be.revertedWith("ERC20InvalidSender");
-  //     });
-  // });
+    it("Should handle zero value approvals correctly", async function () {
+      const wallet1Address = await wallet1.getAddress();
 
-  // describe("Edge cases", function () {
-  //     it("Should fail when transferring to the zero address", async function () {
-  //         const amount = ethers.parseEther("100");
+      await token.approve(wallet1Address, 0);
 
-  //         await expect(
-  //             token.transfer(ethers.constants.AddressZero, amount)
-  //         ).to.be.revertedWith("ERC20InvalidReceiver");
-  //     });
-
-  //     it("Should fail when approving the zero address as spender", async function () {
-  //         const amount = ethers.parseEther("100");
-
-  //         await expect(
-  //             token.approve(ethers.constants.AddressZero, amount)
-  //         ).to.be.revertedWith("ERC20InvalidSpender");
-  //     });
-
-  //     it("Should handle zero value transfers correctly", async function () {
-  //         const addr1Address = await addr1.getAddress();
-  //         const initialBalance = await token.balanceOf(addr1Address);
-
-  //         await token.transfer(addr1Address, 0);
-
-  //         expect(await token.balanceOf(addr1Address)).to.equal(initialBalance);
-  //     });
-
-  //     it("Should handle zero value approvals correctly", async function () {
-  //         const addr1Address = await addr1.getAddress();
-
-  //         await token.approve(addr1Address, 0);
-
-  //         expect(await token.allowance(await owner.getAddress(), addr1Address)).to.equal(0);
-  //     });
-  // });
+      expect(await token.allowance(await owner.getAddress(), wallet1Address)).to.equal(0);
+    });
+  });
 });
